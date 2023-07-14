890a65630c3035229a83880d160c30dadec5e13f
Xzandyaxiz <xyandnotzaxis@gmail.com>
2023-07-14 21:57:54 +0200

    Added writing support and prepared subdir search.

--- main.go
+++ main.go
@@ -6,33 +6,44 @@ import (
 )
 
 type Cursor struct {
+  matches []string
   x int; text string
 }
 
-// For handling inputs
-func input_mgr(cursor Cursor, event termbox.Event) {
+func input_mgr(cursor *Cursor, event termbox.Event) {
   switch event.Type {
-    case termbox.EventKey:
-      
-      // Return in the case of escape being pressed  
-      if event.Key == termbox.KeyEsc {
-        termbox.Close()
-        os.Exit(0)
-      }
+  case termbox.EventKey:
+    if event.Key == termbox.KeyEsc {
+      termbox.Close()
+      os.Exit(0)
+    }
 
-      if event.Ch != 0 {
-        cursor.text += string(event.Ch)
+    if event.Ch != 0 {
+      cursor.text += string(event.Ch)
+    }
+
+    if event.Key == termbox.KeySpace {
+      cursor.text += " "
+    }
+  
+    if event.Key == termbox.KeyBackspace2 {
+      if len(cursor.text) > 0 {
+        cursor.text = cursor.text[:len(cursor.text) - 1]
       }
+    }
   }
 }
+
 
 // Draw the Search field
 func draw_text(cursor Cursor) {
   termbox.SetCell(2, 1, '>', termbox.ColorWhite, termbox.ColorDefault)
+  termbox.SetCell(4, 1, '_', termbox.ColorDefault, termbox.ColorDefault);
   
   // Draw the text from the cursor struct
   for i, char := range cursor.text {
     termbox.SetCell(i+4, 1, char, termbox.ColorDefault, termbox.ColorDefault);
+    termbox.SetCell(i+5, 1, '_', termbox.ColorDefault, termbox.ColorDefault);
   }
 }
 
@@ -45,7 +56,7 @@ func main() {
 
   defer termbox.Close()
 
-  cursor := Cursor {
+  cursor := &Cursor {
     x: 3, 
     text: "",
   }
@@ -53,7 +64,7 @@ func main() {
   // Main loop
   for {
     termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-    draw_text(cursor)
+    draw_text(*cursor)
 
     termbox.Flush()
   
