7d9f9f374a126b691c78d8acdf8499529e510b34
Xzandyaxiz <xyandnotzaxis@gmail.com>
2023-07-17 18:41:00 +0200

    Started working on directory changing system.

--- go.mod
+++ go.mod
@@ -7,5 +7,4 @@ require (
 	github.com/nsf/termbox-go v1.1.1 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 	golang.org/x/sys v0.0.0-20210317225723-c4fcb01b228e // indirect
-	seehuhn.de/go/ncurses v0.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,14 +4,19 @@ import (
   "fmt"; "os"; "strings"
   "github.com/nsf/termbox-go"
   "path/filepath"; "strconv"
+  "syscall"
 )
 
 type Cursor struct {
   matches []string
   x int; text string
+  select_mode bool
+  select_index int
 }
 
 func input_mgr(cursor *Cursor, event termbox.Event) {
+  _, height := termbox.Size()
+
   switch event.Type {
   case termbox.EventKey:
     if event.Key == termbox.KeyEsc {
@@ -19,76 +24,150 @@ func input_mgr(cursor *Cursor, event termbox.Event) {
       os.Exit(0)
     }
 
-    if event.Ch != 0 {
-      cursor.text += string(event.Ch)
+    if !cursor.select_mode {
+      if event.Ch != 0 {
+        cursor.text += string(event.Ch)
+      }
+
+      if event.Key == termbox.KeySpace {
+        cursor.text += " "
+      }
+  
+      if event.Key == termbox.KeyBackspace2 {
+        if len(cursor.text) > 0 {
+          cursor.text = cursor.text[:len(cursor.text) - 1]
+        }
+      }
+    } else {
+      if event.Key == termbox.KeyArrowDown {
+        if cursor.select_index < height - 5 {
+          cursor.select_index ++
+        }
+      }
+
+      if event.Key == termbox.KeyArrowUp {
+        if cursor.select_index > 0 {
+          cursor.select_index --
+        }
+      }
+
+      if event.Key == termbox.KeyEnter {
+        termbox.Close()
+
+        syscall.Chdir(cursor.matches[cursor.select_index])
+        os.Exit(0)
+      }
     }
 
-    if event.Key == termbox.KeySpace {
-      cursor.text += " "
-    }
-  
-    if event.Key == termbox.KeyBackspace2 {
-      if len(cursor.text) > 0 {
-        cursor.text = cursor.text[:len(cursor.text) - 1]
+    if event.Key == termbox.KeyTab {
+      if cursor.select_mode == false {
+        cursor.select_mode = true
+      } else {
+        cursor.select_mode = false
+        cursor.text = ""
       }
     }
   }
 }
 
-func draw_matches(cursor *Cursor) {
+func draw_matches(cursor *Cursor, line_nums bool) {
+  var offset int = 2
+
   for i, str := range cursor.matches {
+    if line_nums {
+      rownum := strconv.Itoa(i)
+      if i >= 10 {
+        offset = 1
+      }
+
+      for x, char := range rownum {
+        termbox.SetCell(x+offset, i+1, char, termbox.ColorYellow, termbox.ColorDefault)
+      }
+    }
+    
     for j, char := range str {
-      termbox.SetCell(j+4, i+3, char, termbox.ColorDefault, termbox.ColorDefault)
+      termbox.SetCell(j+4, i+1, char, termbox.ColorDefault, termbox.ColorDefault)
     }
     
-    rownum := strconv.Itoa(i)
-    
-    for x, char := range rownum {
-      termbox.SetCell(x+1, i+3, char, termbox.ColorYellow, termbox.ColorDefault)
+    // Draw the matches to the screen
+    if cursor.select_mode && cursor.select_index == i {
+      for j, char := range str {
+        termbox.SetCell(j+4, i+1, char, termbox.ColorBlack, termbox.ColorWhite)
+      }
     }
-  } 
+    
+  }
 }
 
-
 // Draw the Search field
-func draw_text(cursor Cursor) {
-  termbox.SetCell(4, 1, '_', termbox.ColorDefault, termbox.ColorDefault); 
+func draw_text(cursor *Cursor) {
+  width, height := termbox.Size();
+
+  cwd, err := os.Getwd()
+  if err != nil {
+    return 
+  }
+  
+  for i := 0; i < width; i++ {
+    termbox.SetCell(i, height-2, ' ', termbox.ColorDefault, termbox.ColorWhite)
+  }
+
+  for i, char := range cwd {
+    termbox.SetCell(i+1, height-2, char,termbox.ColorBlack | termbox.AttrBold, termbox.ColorWhite)
+  }
+
+  if cursor.select_mode {
+    return
+  }
+
+  termbox.SetCell(1, height-1, '_', termbox.ColorDefault | termbox.AttrBold, termbox.ColorDefault); 
+  
+  // Draw underline
+  /*for i := 0; i < 100; i++ {
+    termbox.SetCell(i, 1, '━', termbox.ColorDefault, termbox.ColorDefault | termbox.AttrUnderline)
+  }*/
 
   // Draw the text from the cursor struct
   for i, char := range cursor.text {
-    termbox.SetCell(i+4, 1, char, termbox.ColorDefault, termbox.ColorDefault);
-    termbox.SetCell(i+5, 1, '_', termbox.ColorDefault, termbox.ColorDefault);
+    termbox.SetCell(i+1, height-1, char, termbox.ColorDefault, termbox.ColorDefault);
+    termbox.SetCell(i+2, height-1, '_', termbox.ColorDefault, termbox.ColorDefault);
   }
 }
 
 // Find subdirs with name matching input base path
 func find_subdirs(entrypoints []string, query string, depth int) []string {
-  var results []string
+	var results []string
+  _, height := termbox.Size()
 
-  for _, root := range entrypoints {
-    filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+	for _, root := range entrypoints {
+		filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+      if len(results) > height - 5 {
+        return filepath.SkipDir
+      }
+
       if err != nil {
-        return err
-      }
+				return err
+			}
 
-      if info.IsDir() {
-        if strings.Contains(filepath.Base(path), filepath.Base(query)) {
-          results = append(results, path)
-        }
-      }
+			// Calculate the current depth level by counting path separators.
+			// We add 1 to account for the root directory itself.
+			currentDepth := strings.Count(path[len(root):], string(os.PathSeparator)) + 1
 
-      if depth > 0 || depth == -1 {
-        if depth != -1 {
-          depth--
-        }
-        return nil
-      }
+			if info.IsDir() {
+				if (depth == -1 && currentDepth >= 1) || currentDepth <= depth {
+					if strings.Contains(filepath.Base(path), query) {
+						results = append(results, path) 
+          }
+				} else {
+					return filepath.SkipDir
+				}
+			}
 
-      return filepath.SkipDir
-    })
-  }
+			return nil
+		})
+	}
 
-  return results
+	return results
 }
 
 func main() {
@@ -103,19 +182,20 @@ func main() {
   cursor := &Cursor {
     x: 3, 
     text: "",
+    select_mode: false,
   }
 
-  entrypoints := []string { "/home/xyandzaxis/projects", "/home/xyandzaxis/.config" }
+  entry_points := []string { "/home" }
 
   // Main loop
   for {
     termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 
-    cursor.matches = find_subdirs(entrypoints, cursor.text, 100)
-    draw_text(*cursor)
+    cursor.matches = find_subdirs(entry_points, cursor.text, 6)
     
-    draw_matches(cursor)
-
+    draw_text(cursor)
+    draw_matches(cursor, false)
+    
     termbox.Flush()
   
     event := termbox.PollEvent()
